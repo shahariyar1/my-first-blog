f31edbf90a7581c025246753e945def880c890dd
shahariyar1 <shariaha59@gmail.com>
2023-09-02 08:10:00 +0600

    sign added

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,29 +1,62 @@
 import styled from "@emotion/styled";
 import { Box, Button, TextField, } from "@mui/material";
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Login = () => {
 
     const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
 
+    const[account,toggleAccount]= useState('login');
+
+    const togglesignup = () => {
+       account ==='signup' ? toggleAccount('login') : toggleAccount('signup')
+    }
+
     return (
-        <div>
             <Box>
                 <div className="flex flex-col gap-8 lg:mx-96 my-16 sm:mx-12 bg-white-500 shadow-lg shadow-black-500/50">
                     <img className="w-24 mx-auto my-14" src={imageURL} alt="login" />
+                    
+                    { 
+                    account === 'login' ?
+                            // Login
                     <div className="flex flex-col gap-8 mx-8 mb-6">
                         <TextField id="standard-basic" label="Enter your username" variant="standard" />
                         <TextField id="standard-basic" label="Password" variant="standard" />
                         <Button sx={`background-color: red;
-                        font-size: 16px;&:hover {
-                        background-color: red;`} variant="contained">LOGIN</Button>
-                        <Button>Creat an account</Button>
-                    </div>
-                </div>
+                        font-size: 16px;
+                        text-transform: none;
+                        &:hover {
+                        background-color: red;`} variant="contained">Login</Button>
+                        <Button sx={`
+                            text-transform: none;
 
+                        `}
+                        onClick={() => togglesignup()}
+                        >Creat an account</Button>
+                    </div>
+                        :
+                            //Signed in
+                    <div className="flex flex-col gap-8 mx-8 mb-6">
+                        <TextField id="standard-basic" label="Enter your username" variant="standard" />
+                        <TextField id="standard-basic" label="Enter your username" variant="standard" />
+                        <TextField id="standard-basic" label="Password" variant="standard" />
+                        <Button sx={`background-color: red;
+                        font-size: 16px;
+                        text-transform: none;
+                        &:hover {
+                        background-color: red;`} variant="contained">SignUp</Button>
+                        <Button sx={`
+                            text-transform: none;
+
+                        `}
+                        onClick={() => togglesignup()}
+                        >Already have an account</Button>
+                    </div>
+}
+</div>
             </Box>
-        </div>
     );
 };
 
